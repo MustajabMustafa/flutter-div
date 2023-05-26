4ec6b48afcc0f782d15a279bc97d0e26dca048d0
MustajabMustafa <mustajab.mustafa@gmail.com>
2023-05-27 01:12:13 +0500

    MyFirstCommit

---
+++ firebasepractice/lib/screens/loginView.dart
@@ -0,0 +1,69 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebasepractice/screens/signUpView.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  LoginView({super.key});
+  TextEditingController emailControl = TextEditingController();
+  TextEditingController passControl = TextEditingController();
+  @override
+  login(context) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailControl.text, password: passControl.text);
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => LoginView(),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 255, 249, 249),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Welcome back to mega mall",
+            style: TextStyle(fontSize: 35),
+          ),
+          TextField(
+            controller: emailControl,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Email',
+            ),
+          ),
+          TextField(
+            controller: passControl,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+            ),
+          ),
+          GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SignupView()));
+              },
+              child: const Text("Did not have account Signup?")),
+          ElevatedButton(
+              onPressed: () {
+                login(context);
+              },
+              child: const Text("Login"))
+        ],
+      ),
+    );
+  }
+}
---
+++ firebasepractice/lib/screens/signUpView.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebasepractice/screens/loginView.dart';
+import 'package:flutter/material.dart';
+
+class SignupView extends StatelessWidget {
+  SignupView({super.key});
+  TextEditingController semailControl = TextEditingController();
+  TextEditingController spassControl = TextEditingController();
+  signup(context) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: semailControl.text,
+        password: spassControl.text,
+      );
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TextField(
+            controller: semailControl,
+          ),
+          TextField(
+            controller: spassControl,
+          ),
+          GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginView()));
+              },
+              child: const Text("Do you have account Login")),
+          ElevatedButton(
+              onPressed: () {
+                signup(context);
+              },
+              child: const Text("signup"))
+        ],
+      ),
+    );
+  }
+}
