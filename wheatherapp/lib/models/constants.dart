07058a9b1d0c873f99c77902f1756a3fa27c413d
MustajabMustafa <mustajab.mustafa@gmail.com>
2023-06-24 03:19:52 +0500

    AssiGnment 6

--- wheatherapp/lib/models/constants.dart
+++ wheatherapp/lib/models/constants.dart
@@ -1,1 +1,6 @@
-class Constant {}
+import 'package:flutter/material.dart';
+
+class Constants {
+  final Color primaryColor = const Color(0xff90B2F9);
+  final Color secondaryColor = const Color(0xff90B2F8);
+}
--- wheatherapp/lib/views/get_started.dart
+++ wheatherapp/lib/views/get_started.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:wheatherapp/views/welcome.dart';
+import '../models/constants.dart';
 
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Constants myConstants = Constants();
     Size size = MediaQuery.of(context).size;
-
     return Scaffold(
       body: Container(
         width: size.width,
         height: size.height,
-        color: Colors.red,
+        color: myConstants.primaryColor.withOpacity(.5),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -21,17 +23,23 @@ class GetStarted extends StatelessWidget {
               const SizedBox(
                 height: 30,
               ),
-              Container(
-                height: 50,
-                width: size.width * 0.7,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: const Center(
-                  child: Text(
-                    "Get Started",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => const Welcome()));
+                },
+                child: Container(
+                  height: 50,
+                  width: size.width * 0.7,
+                  decoration: BoxDecoration(
+                    color: myConstants.primaryColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
                   ),
                 ),
               )
