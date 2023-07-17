425c1f38329191b0b90ab99a7cd937473e3c0d2d
MustajabMustafa <mustajab.mustafa@gmail.com>
2023-07-18 00:38:49 +0500

    Stacked- MVVM

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:chat_app/app/app.locator.dart';
 import 'package:chat_app/app/app.router.dart';
-import 'package:chat_app/views/DashBoard.dart';
+//import 'package:chat_app/views/DashBoard.dart';
 //import 'package:chat_app/views/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-void main() {
+void main() async {
+  await setupLocator();
   runApp(const MyApp());
 }
 
@@ -17,6 +19,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         //theme: MyTheme.lightTheme,
         //darkTheme: MyTheme.darkTheme,
+        debugShowCheckedModeBanner: false,
+        title: 'Chat App',
         navigatorKey: StackedService.navigatorKey,
         onGenerateRoute: StackedRouter().onGenerateRoute);
   }
